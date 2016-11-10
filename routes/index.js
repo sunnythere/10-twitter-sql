4f2d095042c0d62b6d52b14bae4810d892c2b280
C.S. Jiang <christine.s.jiang@gmail.com>
2016-11-10 17:30:59 -0500

    post new tweets

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,6 @@ module.exports = function makeRouterWithSockets (io) {
 
   // single-user page
   router.get('/users/:username', function(req, res, next){
-    // var tweetsForName = tweetBank.find({ name: req.params.username });
     client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE name = \'' + req.params.username + '\';', function(err, result) {
       if (err) return next(err);
       var tweets = result.rows;
@@ -35,10 +34,10 @@ module.exports = function makeRouterWithSockets (io) {
         showForm: true
       });
     });
-});
+  });
   // single-tweet page
   router.get('/tweets/:id', function(req, res, next){
-    client.query('SELECT * FROM tweets WHERE id = ' + req.params.id + ';', function(err, result) {
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE tweets.id = ' + req.params.id + ';', function(err, result) {
       if (err) return next(err);
       var tweets = result.rows;
       res.render('index', {
@@ -46,41 +45,30 @@ module.exports = function makeRouterWithSockets (io) {
         tweets: tweets,
         showForm: true
       });
-  });
+    });
   });
 
   // create a new tweet
   router.post('/tweets', function(req, res, next){
-    var nameExists = 0;
-    var userId;
+    var nameExists = false;
+    var UI;
     client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE name = \'' + req.body.name + '\';', function(err, result) {
       if (err) return next(err);
+      console.log(result.rowCount);
       if (result.rowCount > 0) {
-        nameExists = 1;
-
-        userId = result.rows[0].userid;
-      }
+        nameExists = true;
+        UI = result.rows[0].id;
+        client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2) RETURNING id;', [UI, req.body.content], function(err, result) {
+          if (err) return next(err);
+        });
+      } else {
+        client.query('INSERT INTO users (id, name, pictureurl) VALUES (DEFAULT, \'' + req.body.name + '\', DEFAULT) RETURNING id;', function(err, result) {
+          if (err) return next(err);
+          UI = result.rows[0].id;
+          client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2) RETURNING id;', [UI, req.body.content], function(err, result) {if (err) return next(err);});
+      })}
     });
-    if (!nameExists) {
-      //insert into name
-      client.query('INSERT INTO users (id, name, pictureurl) VALUES (DEFAULT, \'' + req.body.name + '\', DEFAULT) RETURNING id;', function(err, result) {if (err) return next(err);
-              console.log(result);
-        userId = result.rows[0].id;})
-
-    }
-    //insert into tweets
-
-    client.query('INSERT INTO tweets (id, userid, content) VALUES ($1, $2, $3) RETURNING id;', ['DEFAULT', userId, '\'' + req.body.content + '\''], function(err, result) {if (err) return next(err);
-    // var newTweet = tweetBank.add(req.body.name, req.body.content);
-    //io.sockets.emit('new_tweet', newTweet);
     res.redirect('/');
   });
-})
-
-  // // replaced this hard-coded route with general static routing in app.js
-  // router.get('/stylesheets/style.css', function(req, res, next){
-  //   res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
-  // });
-
   return router;
-}
+};
